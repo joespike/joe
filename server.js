1a988cfb35cbffc20fa4b736455658eaf29288f0
joespike <34216322+joespike@users.noreply.github.com>
2025-12-18 04:00:17 +0900

    Use environment variable for server port

--- server.js
+++ server.js
@@ -68,4 +68,5 @@ app.post("/concat", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("concat server on :3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("concat server on :" + PORT));
